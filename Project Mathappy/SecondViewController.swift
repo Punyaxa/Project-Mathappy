bc54f9cb16be8f9bbac168d5dc4236fe7a5f768c
下屋敷邦寛 <shimoyashikikunikan@shimoyashikikunikannomacbook-ea.local>
2018-04-28 16:46:31 +0900

    Initial Commit

---
+++ Project Mathappy/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Project Mathappy
+//
+//  Created by 下屋敷邦寛 on 2018/04/28.
+//  Copyright © 2018年 下屋敷邦寛. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
